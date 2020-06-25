e71eec7e9d6a7a880d992fb3ff5b7053cb37e78f
Sword-holder <iamthexzy@gmail.com>
2020-06-25 22:32:00 +0800

    Implement test script

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-def func(**dict):
-    name = list(dict.keys())[0]
-    print(name)
-    print(type(name))
-    
-func(attr='hello')
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser(description='Argument of test script.')
+
+parser.add_argument('--type', type=str, help='test type(options: basic_test, easy_input_test, ...)')
+parser.add_argument('--number', type=int, help='test_script_id')
+
+args = parser.parse_args()
+
+root = 'tests'
+test_type = args.type
+number = args.number
+module_name = test_type + str(number)
+
+path = '.'.join([root, test_type, module_name])
+exec('from {} import {}'.format(path, module_name))
+exec('{}()'.format(module_name))
